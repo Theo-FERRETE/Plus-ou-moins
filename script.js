522f4c565aa4dee63cc929d44c08ea25b57d2c87
Theo-FERRETE <theo.ferrete@laplateforme.io>
2026-04-13 14:55:35 +0200

    playgame

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+function playGame() {
+
 let numMin = prompt("Please enter minimum number", "0");
 let numMax = prompt("Please enter maximum number", "100");
 
@@ -5,28 +7,30 @@ function generateRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-console.log(generateRandomNumber(parseInt(numMin), parseInt(numMax)));
+const randomNumber = generateRandomNumber(parseInt(numMin), parseInt(numMax));
+console.log(randomNumber);
 
-// function enterName(){
-//     return prompt("Please enter your name", "Harry Potter");
-// }
+let userGuess = prompt("Guess the number between " + numMin + " and " + numMax);
 
-// console.log(enterName());
+// Partie conditions
+ while (parseInt(userGuess) !== randomNumber) {
+    if(parseInt(userGuess) > randomNumber){
+      userGuess = prompt("Réessayez, c'est moins");  
+    }
 
-// function enterMin(){
-//     return prompt("Please enter minimum number", "0");
-// }
+    if(parseInt(userGuess) < randomNumber ){
+    userGuess = prompt("Réessayez, c'est plus");
+    }
+ }
 
-// console.log(enterMin());
+ if (parseInt(userGuess) === randomNumber) {
+// alert("Tu as gagné le Tigre !");
+let regame = prompt("Tu as gagné le tigre ! Veux-tu rejouer ?", "");
+if (regame === "oui"){
+    playGame();
+}
 
-// function enterMax(){
-//     return prompt("Please enter maximum number", "100");
-// }
+}
+}
 
-// console.log(enterMax());
-
-// function generateRandomNumber(min, max){
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// console.log(generateRandomNumber (enterMin(), enterMax()));
+playGame();
